9bbdc818b9ac0726b4dbe62d15206900912ddca2
margri764 <62308899+margri764@users.noreply.github.com>
2022-02-22 20:05:37 -0300

    r

--- src/app/peoples/pages/myIndustry/my-industry/my-industry.component.ts
+++ src/app/peoples/pages/myIndustry/my-industry/my-industry.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef, Component, ElementRef, HostListener, OnChanges, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ArtistService } from 'src/app/services/artist/artist.service';
 
@@ -7,19 +7,19 @@ import { ArtistService } from 'src/app/services/artist/artist.service';
   templateUrl: './my-industry.component.html',
   styleUrls: ['./my-industry.component.scss']
 })
-export class MyIndustryComponent implements OnInit {
+export class MyIndustryComponent implements OnInit, AfterViewChecked {
 
 // public ux: boolean= false;
 public editorial: boolean= false;
+public ux: boolean= false;
+public branding: boolean= false;
+public value: string= 'clear';
+public closeEditorial: boolean= false;
 
-@ViewChild('ux',{static:false}) ux: ElementRef | undefined;
 
-@HostListener('click') onMouseClic(){
-  alert('ee')
-  let test= this.ux?.nativeElement.querySelector('.uxa')
-  this.rendered.setStyle(test,'display','none')
 
-}
+
+
 
 
   myForm:FormGroup = this.fb.group({
@@ -34,13 +34,18 @@ public editorial: boolean= false;
   
   constructor(
             private fb : FormBuilder,
-            private artistService : ArtistService,
-            private rendered : Renderer2,
+            private cdRef:ChangeDetectorRef
             
   ) { }
 
   ngOnInit(): void {
   }
+
+  ngAfterViewChecked() {
+    this.cdRef.detectChanges();
+ 
+  }
+
 
   validField( field: string ) {
 
@@ -58,24 +63,57 @@ public editorial: boolean= false;
 
 //   }
 
-  selectedSkill(skill : string){
+  selectSkill(skill : string){
 
     switch( skill ){ 
       case 'ux' :
      
-        this.rendered.setStyle(this.ux?.nativeElement,'backgroundColor','black')
-
+      this.ux=true
   
        break;
 
       case 'editorial' :
+
+        if(this.value !='no se debe enviar'){
+          this.editorial=!this.editorial;
+          this.closeEditorial=true;
+          console.log('valor para mandar a BD','valor: ',this.value)
+      }
+    
+      
+   
+      // if(this.editorial==false){ return} 
+      
+      break;
+
+      case 'branding' :
+        this.branding=true;
+  
+        break;
+    }
+
+  }
+  closeSkill( skill : string){
+    switch( skill ){ 
+      case 'ux' :
+     
+      this.ux=true
+  
+       break;
+
+      case 'editorial' :
+      this.closeEditorial=false;
       this.editorial=true;
+      this.value='no se debe enviar'
+      console.log('no se deberia mandar el valor para mandar a BD','valor: ',this.value)
+   
 
       break;
-       
-      
-     
+
+      case 'branding' :
+        this.branding=true;
   
+        break;
     }
 
   }
